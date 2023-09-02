8addf1f858b262cc417137d9ad6a079a40585913
ruchir-07 <99608006+ruchir-07@users.noreply.github.com>
2023-09-03 03:10:30 +0530

    Cryptocurrencies Done

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,12 +3,13 @@ import millify from 'millify'
 import { Typography, Row, TableColumnGroupType, Statistic, Col } from 'antd'
 import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components'
 const { Title } = Typography;
 
 
 const HomePage = () => {
 
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
   if(isFetching) return 'loading'
@@ -23,6 +24,20 @@ const HomePage = () => {
         <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+        <Title level={2} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+      </div>
+
+      <Cryptocurrencies simplified />
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latesr Crypto News</Title>
+        <Title level={2} className="show-more"><Link to="/news">Show More</Link></Title>
+      </div>
+
+      <News simplified />
     </>
   )
 }
